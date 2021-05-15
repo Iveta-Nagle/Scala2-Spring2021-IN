64e65fceb30ed56dadcaff57f1611b817cedbe0f
Iveta <iveta.nagled@gmail.com>
2021-05-15 22:45:02 +0300

    Crush task working

--- src/main/scala/Crush.scala
+++ src/main/scala/Crush.scala
@@ -1,39 +1,47 @@
 import com.github.ivetan.Utilities
 
+import scala.collection.mutable.ArrayBuffer
+
 object Crush extends App {
-  //TODO Solve the following problem
+
   //https://www.hackerrank.com/challenges/crush/problem
-  //TODO read and parse ../resources/crush1.txt
-  //TODO read and parse ../resources/crush2.txt
-  //you need to perform 3 operations
-  //and find the maximum in the new array
 
-  //think how well this would work on large values of operations like
-  //1000 1000
+  val folderName = "./src/resources"
+  val filePaths = Utilities.getFileNames(folderName, prefix="crush", suffix = "xt")
+  filePaths.foreach(println)
 
-  val filePath = "./src/resources/crush1.txt"
-  val lines = Utilities.getLinesFromFile(filePath)
-  val numbOfZeros = lines(0).split(" ").toSeq.head.toInt
-  val numbOfOperations = lines(0).split(" ").toSeq.last.toInt
-  println(numbOfZeros)
-  println(numbOfOperations)
-  val array0 = (1 to numbOfZeros).map(n => 0)
-  array0.foreach(print)
+  def getColumn(lines: Array[String], columnIndex: Int): Array[Int] = {
+    val cols = lines.map(line => line.split(" ").map(_.trim))
+    val columnArray = cols.tail.map(line => line(columnIndex).toInt)
+    columnArray
+  }
 
-  println()
-  val array1Data = lines(1).split(" ").toIndexedSeq
-  val array1Start = array1Data(0).toInt
-  val array1End = array1Data(1).toInt
-  val array1Sum = array1Data(2).toInt
+  def getArrayMaxValue(filePath: String) : Long = {
+    val lines = Utilities.getLinesFromFile(filePath)
+    val nrOfElements = lines(0).split(" ").toSeq.head.toInt
+    val numbOfOperations = lines(0).split(" ").toSeq.last.toInt
 
-  val array1 = array0.map(n => if (array0.indexOf(n)+1<array1Start) 0 else n+100 )
-  array1.foreach(println)
-  //  println(array1Start)
-  //  println(array1End)
-  //val array1 = array0.map
-  //  val array1 = (1 to numbOfZeros).map(n => ).toSeq
-  //  val stringEnd = numbOfString+1
-  //  val strings = lines.slice(1, stringEnd)
-  //  val queries = lines.slice(stringEnd+1, lines.length)
+    val myArray = ArrayBuffer.fill(nrOfElements)(0)
+
+    val startIndexes = getColumn(lines, 0)
+    val endIndexes = getColumn(lines, 1)
+    val valuesToAdd = getColumn(lines, 2)
+
+    var max = 0
+
+    for (n <- 0 until numbOfOperations) {
+        for (i <- startIndexes(n) -1 until endIndexes(n)) {
+          myArray.update(i, myArray(i) + valuesToAdd(n))
+        if (myArray(i) > max) max = myArray(i)
+      }
+    }
+    max
+  }
+
+  def printResult(max: Long, filePath: String): Unit = {
+    println(s"Max value after manipulations from file $filePath is $max")
+  }
+
+  filePaths.foreach(filePath => printResult(getArrayMaxValue(filePath), filePath))
 
 }
